4fe97a8313de5737ff3ce68d401273702054d785
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2025-03-12 16:53:14 +0500

    varable latex update

--- user/views.py
+++ user/views.py
@@ -36,6 +36,7 @@ def scopus(request):
     return render(request,'scopus.html')
 
 def function1(data):
+    # O'zgaruvchilarni olish va boshlang'ich shartlar
     k0 = float(data.get('permeability', 1e-13))
     myu0 = float(data.get('fluid_viscosity', 1e-2))
     P0 = float(data.get('pressure', 5e+5))
@@ -44,7 +45,6 @@ def function1(data):
     lv = float(data.get('relaxation_time1', 1000))
     lp = float(data.get('relaxation_time2', 500))
     betta = float(data.get('fractional_derivative_time1', 0.9))
-    alf = float(data.get('fractional_derivative_time2', 0.7))
     bet = 1 + betta
     tau = float(data.get('grid_step_direction1', 10))
     h = float(data.get('grid_step_direction2', 0.05))
@@ -52,68 +52,76 @@ def function1(data):
     T = int(tMax / tau) + 1
     N = int(L / h) + 1
 
-    P = np.zeros((T, N))
-    P[:, 0] = P0
-    P[0, :] = 0
-    P[:, -1] = 0
-    U = np.zeros((T, N))
+    alf_values = [1, 0.7, 0.5]
+    results = {}
+
     kap = k0 / (myu0 * bet_y)
 
-    for i in range(N):
-        P[1, i] = P[0, i]
+    # Hisoblashni takrorlash
+    for alf in alf_values:
+        P = np.zeros((T, N))
+        P[:, 0] = P0
+        U = np.zeros((T, N))
 
-    kv = lv / ((tau ** bet) * gamma(3 - bet))
-    kp = lp / ((tau ** alf) * (h ** 2) * gamma(2 - alf))
-    kv1 = lv / ((tau ** betta) * gamma(2 - betta))
-    kp2 = lp / ((tau ** alf) * gamma(2 - alf))
-    A = kap / h ** 2 + kap * kp
-    B = 1 / tau + kv + 2 * kap / h ** 2 + 2 * kap * kp
-    C = kap / h ** 2 + kap * kp
-    alpha = np.zeros(N)
-    beta = np.zeros(N)
-    beta[1] = P0
+        kv = lv / ((tau ** bet) * gamma(3 - bet))
+        kp = lp / ((tau ** alf) * (h ** 2) * gamma(2 - alf))
+        kv1 = lv / ((tau ** betta) * gamma(2 - betta))
+        kp2 = lp / ((tau ** alf) * gamma(2 - alf))
 
-    for j in range(1, T - 1):
-        for i in range(1, N - 1):
-            sv = 0
+        A = kap / h ** 2 + kap * kp
+        B = 1 / tau + kv + 2 * kap / h ** 2 + 2 * kap * kp
+        C = kap / h ** 2 + kap * kp
+
+        alpha = np.zeros(N)
+        beta = np.zeros(N)
+        beta[1] = P0
+
+        # Asosiy hisoblash tsikli
+        for j in range(1, T - 1):
+            sv = np.zeros(N)
+            sp1 = np.zeros(N)
+            sp2 = np.zeros(N)
+            sp3 = np.zeros(N)
+            st = np.zeros(N)
+
             for k in range(1, j):
-                sv += (P[k + 1, i] - 2 * P[k, i] + P[k - 1, i]) * ((j - k + 1) ** (2 - bet) - (j - k) ** (2 - bet))
-            sp1 = 0
-            sp2 = 0
-            sp3 = 0
-            st = 0
+                delta = (j - k + 1) ** (2 - bet) - (j - k) ** (2 - bet)
+                sv[1:-1] += (P[k + 1, 1:-1] - 2 * P[k, 1:-1] + P[k - 1, 1:-1]) * delta
+
             for k in range(j):
-                sp1 += (P[k + 1, i + 1] - P[k, i + 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
-                sp2 += (P[k + 1, i] - P[k, i]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
-                sp3 += (P[k + 1, i - 1] - P[k, i - 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
-                st += (U[k + 1, i] - U[k, i]) * ((j - k + 1) ** (1 - betta) - (j - k) ** (1 - betta))
+                delta_alf = (j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf)
+                delta_betta = (j - k + 1) ** (1 - betta) - (j - k) ** (1 - betta)
 
-        for i in range(1, N - 1):
-            F = (P[j, i] / tau - kv * sv + 2 * kv * P[j, i] - kv * P[j - 1, i] +
-                 kp * kap * sp1 - kap * kp * P[j, i + 1] - 2 * kap * kp * sp2 +
-                 2 * kap * kp * P[j, i] + kap * kp * sp3 - kap * kp * P[j, i - 1])
+                sp1[1:-1] += (P[k + 1, 2:] - P[k, 2:]) * delta_alf
+                sp2[1:-1] += (P[k + 1, 1:-1] - P[k, 1:-1]) * delta_alf
+                sp3[1:-1] += (P[k + 1, :-2] - P[k, :-2]) * delta_alf
+                st[1:-1] += (U[k + 1, 1:-1] - U[k, 1:-1]) * delta_betta
 
-            alpha[i + 1] = C / (B - A * alpha[i])
-            beta[i + 1] = (F + A * beta[i]) / (B - A * alpha[i])
+            # Tridiagonal sistemani hal qilish
+            F = (P[j, 1:-1] / tau - kv * sv[1:-1] + 2 * kv * P[j, 1:-1] - kv * P[j - 1, 1:-1] +
+                 kp * kap * sp1[1:-1] - kap * kp * P[j, 2:] - 2 * kap * kp * sp2[1:-1] +
+                 2 * kap * kp * P[j, 1:-1] + kap * kp * sp3[1:-1] - kap * kp * P[j, :-2])
 
-        P[j + 1, -1] = 0
-        for i in range(N - 2, -1, -1):
-            P[j + 1, i] = alpha[i + 1] * P[j + 1, i + 1] + beta[i + 1]
+            alpha[2:] = C / (B - A * alpha[1:-1])
+            beta[2:] = (F + A * beta[1:-1]) / (B - A * alpha[1:-1])
 
-        for i in range(N - 1):
-            U[j + 1, i] = abs((-k0 * (P[j + 1, i + 1] - P[j + 1, i] +
-                                         kp2 * (sp1 + P[j + 1, i + 1] - P[j, i + 1] - sp2 -
-                                         P[j + 1, i] + P[j, i])) / (myu0 * h * (1 + kv1)) -
-                                         (kv1 * st - kv1 * U[j, i]) / (1 + kv1)))
+            P[j + 1, -1] = 0
+            for i in range(N - 2, 0, -1):
+                P[j + 1, i] = alpha[i + 1] * P[j + 1, i + 1] + beta[i + 1]
 
-        U[j + 1, -1] = U[j + 1, -2]
+            # U ni hisoblash
+            U[j + 1, :-1] = np.abs((-k0 * (P[j + 1, 1:] - P[j + 1, :-1] +
+                                           kp2 * (sp1[1:] + P[j + 1, 1:] - P[j, 1:] - sp2[1:] -
+                                                  P[j + 1, :-1] + P[j, :-1])) / (myu0 * h * (1 + kv1)) -
+                                           (kv1 * st[1:] - kv1 * U[j, :-1]) / (1 + kv1)))
+            U[j + 1, -1] = U[j + 1, -2]
 
-    x = np.arange(N) * h
-    y = P[T - 2, :]
+        x = np.arange(N) * h
+        y = P[T - 2, :]
 
-    return {'x': x.tolist(), 'y': y.tolist()}
+        results[alf] = {'x': x.tolist(), 'y': y.tolist()}
 
-
+    return results
 def function2(data):
     return data
 
