2536a5ebf3e346ff813bd1d20780cb0f3bddb2f1
shoyimobloqulov <obloqulovshoyim1@gmail.com>
2025-04-01 17:17:33 +0500

    fixed code

---
+++ dev1.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import gamma
+
+def filtration():
+    k0 = 1e-13
+    myu0 = 1e-2
+    P0 = 5e+5
+    bet_y = 3e-10
+
+    tMax = 3600
+
+    lv = 1000
+    lp = 500
+    betta = 1
+    alf = 0.7
+
+    bet = 1 + betta
+
+    tau = 100
+    h = 0.05
+    L = 40
+    T = int(tMax / tau) + 1
+    N = int(L / h) + 1
+
+    # Initial and boundary conditions
+    P = np.zeros((T, N))
+    P[:, 0] = P0
+    P[0, :] = 0
+    P[:, -1] = 0
+    U = np.zeros((T, N))
+    kap = k0 / (myu0 * bet_y)
+
+    for i in range(N):
+        P[1, i] = P[0, i]
+
+    kv = lv / ((tau ** bet) * gamma(3 - bet))
+    kp = lp / ((tau ** alf) * (h ** 2) * gamma(2 - alf))
+
+    kv1 = lv / ((tau ** betta) * gamma(2 - betta))  # velocity
+    kp2 = lp / ((tau ** alf) * gamma(2 - alf))  # velocity
+
+    A = kap / h ** 2 + kap * kp
+    B = 1 / tau + kv + 2 * kap / h ** 2 + 2 * kap * kp
+    C = kap / h ** 2 + kap * kp
+    alpha = np.zeros(N)
+    beta = np.zeros(N)
+    beta[1] = P0
+
+    for j in range(1, T - 1):
+        for i in range(1, N - 1):
+            sv = 0
+            for k in range(1, j):
+                sv += (P[k + 1, i] - 2 * P[k, i] + P[k - 1, i]) * ((j - k + 1) ** (2 - bet) - (j - k) ** (2 - bet))
+
+            sp1 = 0
+            sp2 = 0
+            sp3 = 0
+            st = 0
+            for k in range(j):
+                sp1 += (P[k + 1, i + 1] - P[k, i + 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
+                sp2 += (P[k + 1, i] - P[k, i]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
+                sp3 += (P[k + 1, i - 1] - P[k, i - 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
+                st += (U[k + 1, i] - U[k, i]) * ((j - k + 1) ** (1 - betta) - (j - k) ** (1 - betta))  # velocity
+
+        for i in range(1, N - 1):
+            F = (P[j, i] / tau - kv * sv + 2 * kv * P[j, i] - kv * P[j - 1, i] + kp * kap * sp1 - kap * kp * P[j, i + 1] - 2 * kap * kp * sp2 + 2 * kap * kp * P[j, i] + kap * kp * sp3 - kap * kp * P[j, i - 1])
+            alpha[i + 1] = C / (B - A * alpha[i])
+            beta[i + 1] = (F + A * beta[i]) / (B - A * alpha[i])
+
+        P[j + 1, -1] = 0
+        for i in range(N - 2, -1, -1):
+            P[j + 1, i] = alpha[i + 1] * P[j + 1, i + 1] + beta[i + 1]
+
+        for i in range(N - 1):
+            U[j + 1, i] = abs((-k0 * (P[j + 1, i + 1] - P[j + 1, i] + kp2 * (sp1 + P[j + 1, i + 1] - P[j, i + 1] - sp2 - P[j + 1, i] + P[j, i])) / (myu0 * h * (1 + kv1)) - (kv1 * st - kv1 * U[j, i]) / (1 + kv1))  # velocity
+
+        U[j + 1, -1] = U[j + 1, -2]  # velocity
+
+    # Plotting
+    plt.plot(np.arange(N) * h, P[T - 1, :])
+    plt.show()
+
+filtration()
--- user/views.py
+++ user/views.py
@@ -338,60 +338,94 @@ def filtration_api(request):
 
     lambda_p = float(request.GET.get('relaxation_time2', 0))  
     lambda_v = float(request.GET.get('relaxation_time1', 0))
+    Tmax = float(request.GET.get('maximum_time', 3600))
 
     result = filtration1(
         alf=alf,
+        tMax=Tmax,
         k0=float(request.GET.get('permeability', 1e-13)),
         myu0=float(request.GET.get('fluid_viscosity', 1e-2)),
         P0=float(request.GET.get('pressure', 5e+5)),
         bet_y=float(request.GET.get('elastic_capacity_coefficient', 3e-10)),
         lv=lambda_v,
         lp=lambda_p,
+        betta=betta,
         tau=float(request.GET.get('grid_step_direction1', 10)),
         h=float(request.GET.get('grid_step_direction2', 0.05)),
-        betta=betta
     )
     return JsonResponse({'result': result})
 
 def filtration1(alf, tMax=3600, k0=1e-13, myu0=1e-2, P0=5e+5, bet_y=3e-10, lv=0, lp=500, betta=1, tau=10, h=0.05, L=40):
+    k0 = k0
+    myu0 = myu0
+    P0 = P0
+    bet_y = bet_y
+
+    tMax = tMax
+
+    lv = lv
+    lp = lp
+    betta = betta
+    alf = alf
+
     bet = 1 + betta
-    T = int(tMax / tau + 1)
-    N = int(L / h + 1)
+
+    tau = tau
+    h = h
+    L = L
+    T = int(tMax / tau) + 1
+    N = int(L / h) + 1
+
+    # Initial and boundary conditions
     P = np.zeros((T, N))
-    U = np.zeros((T, N))
     P[:, 0] = P0
+    P[0, :] = 0
+    P[:, -1] = 0
+    U = np.zeros((T, N))
     kap = k0 / (myu0 * bet_y)
+
+    for i in range(N):
+        P[1, i] = P[0, i]
+
     kv = lv / ((tau ** bet) * gamma(3 - bet))
     kp = lp / ((tau ** alf) * (h ** 2) * gamma(2 - alf))
-    kv1 = lv / ((tau ** betta) * gamma(2 - betta))
-    kp2 = lp / ((tau ** alf) * gamma(2 - alf))
+
+    kv1 = lv / ((tau ** betta) * gamma(2 - betta))  # velocity
+    kp2 = lp / ((tau ** alf) * gamma(2 - alf))  # velocity
+
     A = kap / h ** 2 + kap * kp
     B = 1 / tau + kv + 2 * kap / h ** 2 + 2 * kap * kp
     C = kap / h ** 2 + kap * kp
     alpha = np.zeros(N)
     beta = np.zeros(N)
     beta[1] = P0
-    
+
     for j in range(1, T - 1):
         for i in range(1, N - 1):
-            sv = sum((P[k + 1, i] - 2 * P[k, i] + P[k - 1, i]) * ((j - k + 1) ** (2 - bet) - (j - k) ** (2 - bet)) for k in range(1, j))
-            sp1 = sum((P[k + 1, i + 1] - P[k, i + 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf)) for k in range(j))
-            sp2 = sum((P[k + 1, i] - P[k, i]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf)) for k in range(j))
-            sp3 = sum((P[k + 1, i - 1] - P[k, i - 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf)) for k in range(j))
-            st = sum((U[k + 1, i] - U[k, i]) * ((j - k + 1) ** (1 - betta) - (j - k) ** (1 - betta)) for k in range(j))
-        
+            sv = 0
+            for k in range(1, j):
+                sv += (P[k + 1, i] - 2 * P[k, i] + P[k - 1, i]) * ((j - k + 1) ** (2 - bet) - (j - k) ** (2 - bet))
+            sp1 = 0
+            sp2 = 0
+            sp3 = 0
+            st = 0
+            for k in range(j):
+                sp1 += (P[k + 1, i + 1] - P[k, i + 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
+                sp2 += (P[k + 1, i] - P[k, i]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
+                sp3 += (P[k + 1, i - 1] - P[k, i - 1]) * ((j - k + 1) ** (1 - alf) - (j - k) ** (1 - alf))
+                st += (U[k + 1, i] - U[k, i]) * ((j - k + 1) ** (1 - betta) - (j - k) ** (1 - betta))  # velocity
+
         for i in range(1, N - 1):
             F = (P[j, i] / tau - kv * sv + 2 * kv * P[j, i] - kv * P[j - 1, i] + kp * kap * sp1 - kap * kp * P[j, i + 1] - 2 * kap * kp * sp2 + 2 * kap * kp * P[j, i] + kap * kp * sp3 - kap * kp * P[j, i - 1])
             alpha[i + 1] = C / (B - A * alpha[i])
             beta[i + 1] = (F + A * beta[i]) / (B - A * alpha[i])
 
-        P[j + 1, N - 1] = 0
-        for i in range(N - 2, 0, -1):
+        P[j + 1, -1] = 0
+        for i in range(N - 2, -1, -1):
             P[j + 1, i] = alpha[i + 1] * P[j + 1, i + 1] + beta[i + 1]
-        
+
         for i in range(N - 1):
-            U[j + 1, i] = abs((-k0 * (P[j + 1, i + 1] - P[j + 1, i] + kp2 * (sp1 + P[j + 1, i + 1] - P[j, i + 1] - sp2 - P[j + 1, i] + P[j, i])) / (myu0 * h * (1 + kv1))) - (kv1 * st - kv1 * U[j, i]) / (1 + kv1))
-        
-        U[j + 1, N - 1] = U[j + 1, N - 2]
-    
-    return {"pressure": P[T - 2, :].tolist(), "velocity": (np.arange(N) * h).tolist()}
+            U[j + 1, i] = abs((-k0 * (P[j + 1, i + 1] - P[j + 1, i] + kp2 * (sp1 + P[j + 1, i + 1] - P[j, i + 1] - sp2 - P[j + 1, i] + P[j, i])) / (myu0 * h * (1 + kv1)) - (kv1 * st - kv1 * U[j, i]) / (1 + kv1)))  # velocity
+
+        U[j + 1, -1] = U[j + 1, -2]  # velocity
+    return {"pressure": P[T - 1, :].tolist(), "velocity": (np.arange(N) * h).tolist()}
